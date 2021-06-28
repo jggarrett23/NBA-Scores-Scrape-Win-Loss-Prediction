14fb9bc81ed8a49d2a3d74da53a051c2f103d1b7
Jordan Garrett <owner@Oracle.local>
2021-06-28 15:21:45 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sportsWebScrape',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Jordan Garrett',
+    author_email='jordangarrett@ucsb.edu',
+    description=''
+)
